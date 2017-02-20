ab95ac0c923cacb58d7ef7d5ac8466e03ab2c2b1
lewandowski_marcin@hotmail.com <zladupa1>
2017-02-20 14:20:58 +0100

    first commit

---
+++ Assets/Scripts/carController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour
+{
+
+    public float carSpeed = 4;
+    Vector3 position;
+    private float xMin;
+    private float xMax;
+
+    // Use this for initialization
+    void Start()
+    {
+        position = new Vector3(GetComponent<Renderer>().transform.position.x, GetComponent<Renderer>().transform.position.y);
+        xMin = -1.4f;
+        xMax = 1.4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+        if (position.x < xMin)
+        {
+            position.x = xMin;
+        }
+        else if (position.x > xMax)
+        {
+            position.x = xMax;
+        }
+
+        transform.position = position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy Car")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/casSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class casSpawner : MonoBehaviour {
+
+    public GameObject car;
+    public float maxPos = 2.2f;
+    public float delayTimer = 2f;
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+
+        timer = delayTimer;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            Vector3 carPos = new Vector3(Random.Range(-1.40f, 1.40f), transform.position.y, transform.position.z);
+            Instantiate(car, carPos, transform.rotation);
+
+            timer = delayTimer;
+        }
+        
+    }
+}
